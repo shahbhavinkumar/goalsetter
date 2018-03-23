2fcf09acc96c75c3978e99f954187c6a750b458a
shahbhavinkumar <shahbhavinkumar@gmail.com>
2018-03-23 14:53:04 -0500

    adding ninject

---
+++ GoalSetter/Bindings.cs
@@ -0,0 +1,17 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoalSetter
+{
+    public class Bindings : NinjectModule
+
+    {
+        public override void Load()
+        {
+            Bind<goalsetterEntities>().To<goalsetterEntities>();
+        }
+    }
+}
