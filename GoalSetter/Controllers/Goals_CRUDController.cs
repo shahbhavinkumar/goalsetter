c3d82cb027c0714cc0e688846416dfdcbdc22563
shahbhavinkumar <shahbhavinkumar@gmail.com>
2018-03-25 19:29:43 -0500

    changes

--- GoalSetter/Controllers/Goals_CRUDController.cs
+++ GoalSetter/Controllers/Goals_CRUDController.cs
@@ -9,12 +9,7 @@ namespace GoalSetter.Controllers
 {
     public class Goals_CRUDController : Controller
     {
-        private readonly IGoalsService_CRUD _service;
 
-        public Goals_CRUDController(IGoalsService_CRUD service)
-        {
-            _service = service;
-        }
         
     }
 }
